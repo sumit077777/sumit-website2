5f7fe1a6ec35b4ec4d1bf1d4444d93991d09b903
sumit077777 <91362265+sumit077777@users.noreply.github.com>
2023-04-23 07:19:36 +0000

    dynamic data has been added

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Bengaluru',
+  'salary': 1000000
+}, {
+  'id': 2,
+  'title': 'Front End Engineer',
+  'location': 'Remote',
+  'salary': 1000000
+}, {
+  'id': 3,
+  'title': 'Backend-Engineer',
+  'location': 'Noida',
+  'salary': 10000
+}, {
+  'id': 4,
+  'title': 'Data scientist',
+  'location': 'Bengaluru',
+}]
 
 
 @app.route("/")
 def hello_world():
-  return "<p> Hello jovian</p>"
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
